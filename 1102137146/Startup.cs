0917437d6edad804eb7e5573d09a35db473bc8e2
1102137146 <1102137146@gm.kuas.edu.tw>
2016-03-24 02:01:05 +0800

    first

---
+++ 1102137146/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1102137146.Startup))]
+namespace _1102137146
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
